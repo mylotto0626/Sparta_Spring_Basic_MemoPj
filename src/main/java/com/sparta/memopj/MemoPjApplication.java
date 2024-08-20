2839175e2a76b825d95a0489ef426f1d9e70951d
mylotto0626 <gupy1998@gmail.com>
2024-08-20 12:21:37 +0900

    init commit

---
+++ src/main/java/com/sparta/memopj/MemoPjApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.memopj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoPjApplication.class, args);
+	}
+
+}
